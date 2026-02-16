34a78843e0c02484095e166a556e5b38c3d3b827
Spongy163 <drilbd01@pfw.edu>
2026-02-16 02:33:58 -0500

    put instance data fields for DailyNutrition

--- src/calorieTrackerPrototype/DailyNutrition.java
+++ src/calorieTrackerPrototype/DailyNutrition.java
@@ -1,5 +1,35 @@
+/* Brighton Drill
+ * Title: Daily Nutriition Class
+ * Date: 2/16/2026
+ * Description: An object that stores the date, calories, and protein of a day. 
+ */
+
 package calorieTrackerPrototype;
 
-public class DailyNutrition {
+import java.time.LocalDate;
 
+public class DailyNutrition {
+	
+	/* INSTANCE DATA FIELDS
+	 * -calories:int = stores the calorie intake
+	 * -protein:int = stores protein intake in grams
+	 * -date:LocalDate = stores the date
+	 */
+	
+	private int calories;
+	private int protein;
+	private LocalDate date;
+	
+	/* CONSTRUCTORS
+	 * +DailyNutrition() = no argument constructor
+	 */
+	
+	DailyNutrition() {
+		this.calories = 0;
+		this.protein = 0;
+		date.now();
+	}
+	
+	
+	
 }
