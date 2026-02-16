89fe0709c35e2326cac77db09e8ac1cb4db3ab53
Spongy163 <drilbd01@pfw.edu>
2026-02-16 03:01:31 -0500

    Finished writing the DailyNutrition Class

--- src/calorieTrackerPrototype/DailyNutrition.java
+++ src/calorieTrackerPrototype/DailyNutrition.java
@@ -27,9 +27,68 @@ public class DailyNutrition {
 	DailyNutrition() {
 		this.calories = 0;
 		this.protein = 0;
-		date.now();
+		this.date = LocalDate.now();
+	}
+
+	
+	//GETTER AND SETTER
+	
+	/**
+	 * @return the date
+	 */
+	public LocalDate getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date the date to set
+	 */
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return the calories
+	 */
+	public int getCalories() {
+		return calories;
+	}
+
+	/**
+	 * @return the protein
+	 */
+	public int getProtein() {
+		return protein;
 	}
 	
+	/* METHODS
+	 * +addCalories(calories:int):void = increment the calories (negative to decrement)
+	 * +addProtein(protein:int):void = increment the protein (negative to decrement)
+	 * +resetCalories():void = resets calories to zero
+	 * +resetProtein():void = resets the protein to zero
+	 */
 	
+	
+	/**
+	 * @param calories the calories to increment by
+	 */
+	public void addCalories(int calories) {
+		this.calories = this.calories + calories;
+	}
+	
+	/**
+	 * @param protein the protein to increment by
+	 */
+	public void addProtein(int protein) {
+		this.protein = this.protein + protein;
+	}
+	
+	public void resetCalories() {
+		this.calories = 0;
+	}
+	
+	public void resetProtein() {
+		this.protein = 0;
+	}
 	
 }
