cbecc90141e30fc74684b8b516338512e3e85430
Spongy163 <drilbd01@pfw.edu>
2026-02-16 04:05:09 -0500

    Started work on the journal class

---
+++ src/calorieTrackerPrototype/Journal.java
@@ -0,0 +1,30 @@
+/* Brighton Drill
+ * Title: Journal
+ * Date: 2/16/2026
+ * Description: A journal of
+ */
+
+package calorieTrackerPrototype;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.ArrayList;
+
+public class Journal {
+
+	
+	/* INSTANCE METHODS
+	 * -calendar:Map<LocalDate, DailyNutrition> = stores daily nutritions 
+	 * -foods:ArrayList<Food> = stores all of the foods
+	 */
+	
+	private Map<LocalDate, DailyNutrition> calendar;
+	private ArrayList<Food> foods;
+	
+	/* CONSTRUCTORS
+	 * +Journal() = 
+	 */
+	
+	
+	
+}
